753a3a1a9b83a1b91fef2352cb446dafc8f57c92
Christof145 <43606559+Christof145@users.noreply.github.com>
2023-02-12 14:47:37 +0100

    Create initial app

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: '#3ca832',}}>Hello</Text>
       <StatusBar style="auto" />
+      <View style={styles.buttonContainer}>
+        <StatusBar style="auto" />
+        <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+          <Text style={{color: '#3ca832',}}>Get a cat!</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    borderRadius: 10,
+    height: 20,
+    width: 50,
+    marginHorizontal: 30,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'centre',
+    justifyContent: 'centre',
+    flexDirection: 'row',
+  }
+
 });
